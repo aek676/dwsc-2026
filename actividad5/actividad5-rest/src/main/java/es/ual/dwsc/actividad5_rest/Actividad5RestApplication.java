60cbcd5128b53bde633b8b9d48fcd57cd76292c9
aek676 <aek676@inlumine.ual.es>
2026-03-28 19:49:15 +0100

    feat(rest): initialize Spring Boot REST project
    Add initial scaffold for actividad5-rest Spring Boot project. Includes
    Maven wrapper, basic application class, test, and minimal configuration.
    Project dependencies cover Spring Data REST, WebMVC, and HAL Explorer.
    Also includes IDE and build tool ignore files.

---
+++ actividad5/actividad5-rest/src/main/java/es/ual/dwsc/actividad5_rest/Actividad5RestApplication.java
@@ -0,0 +1,13 @@
+package es.ual.dwsc.actividad5_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad5RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad5RestApplication.class, args);
+	}
+
+}
---
+++ actividad5/actividad5-rest/src/test/java/es/ual/dwsc/actividad5_rest/Actividad5RestApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ual.dwsc.actividad5_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad5RestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
