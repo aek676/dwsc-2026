83e3e07b447d8d1a54fd8eb91ee9f0360d5286c6
aek676 <aek676@inlumine.ual.es>
2026-03-28 19:49:03 +0100

    feat(jpa): add Spring Boot JPA project scaffold
    Add initial scaffold for actividad5-jpa module using Spring Boot with
    JPA and PostgreSQL support. Includes Maven wrapper, basic application
    class, test, and minimal configuration. Project dependencies cover
    Spring Data JPA, WebMVC, Lombok, and PostgreSQL. Also includes IDE and
    build tool ignores.

---
+++ actividad5/actividad5-jpa/src/main/java/es/ual/dwsc/actividad5/Actividad5JpaApplication.java
@@ -0,0 +1,13 @@
+package es.ual.dwsc.actividad5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Actividad5JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Actividad5JpaApplication.class, args);
+	}
+
+}
---
+++ actividad5/actividad5-jpa/src/test/java/es/ual/dwsc/actividad5/Actividad5JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ual.dwsc.actividad5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Actividad5JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
