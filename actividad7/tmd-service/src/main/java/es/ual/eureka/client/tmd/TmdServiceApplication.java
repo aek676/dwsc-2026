cb6318973bd2039e0eb428fe35b2d231b1222654
aek676 <aek676@inlumine.ual.es>
2026-04-20 21:28:19 +0200

    feat(tmd-service): initialize Spring Boot Eureka client
    Add initial project structure for tmd-service as a Spring Boot
    application
    configured as a Eureka client. Includes Maven wrapper, basic
    dependencies,
    application configuration, and a sample test. This sets up the
    foundation
    for further microservice development and service discovery integration.

---
+++ actividad7/tmd-service/src/main/java/es/ual/eureka/client/tmd/TmdServiceApplication.java
@@ -0,0 +1,13 @@
+package es.ual.eureka.client.tmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdServiceApplication.class, args);
+	}
+
+}
---
+++ actividad7/tmd-service/src/test/java/es/ual/eureka/client/tmd/TmdServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ual.eureka.client.tmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmdServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
