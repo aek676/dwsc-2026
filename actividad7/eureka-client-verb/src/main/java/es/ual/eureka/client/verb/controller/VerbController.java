74893a9935e3454eeb73a43424c27189c7ca5c67
aek676 <aek676@inlumine.ual.es>
2026-04-20 16:44:44 +0200

    feat: add eureka client verb Spring Boot service
    Added a new Spring Boot project "eureka-client-verb" with Eureka client
    integration. The service provides a REST endpoint that returns a random
    verb from a configurable list, supporting different profiles for
    positive and negative verbs. Includes Maven wrapper, configuration,
    controller, and basic tests.

---
+++ actividad7/eureka-client-verb/src/main/java/es/ual/eureka/client/verb/controller/VerbController.java
@@ -0,0 +1,25 @@
+package es.ual.eureka.client.verb.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class VerbController {
+
+  @Value("${words}")
+  private String words;
+
+  @GetMapping
+  public ResponseEntity<String> getVerb() {
+    String[] wordsArray = words.split(",");
+    int index = (int) Math.round(Math.random() * (wordsArray.length - 1));
+
+    return new ResponseEntity<>(wordsArray[index], HttpStatus.OK);
+  }
+
+}
---
+++ actividad7/eureka-client-verb/src/test/java/es/ual/eureka/client/verb/VerbApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ual.eureka.client.verb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
